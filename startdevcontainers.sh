c590ba9e152748bdd58727661533669657eeb596
frmauro <frmauro8@gmail.com>
2022-04-26 15:39:24 -0300

    first commit

---
+++ startdevcontainers.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+########################################################
+
+## Shell Script to Start Docker Container, DEV.   
+
+########################################################
+
+
+echo "Start the docker container"
+
+result=$( docker ps -q -f name=mongodb )
+if [[ $? -eq 0 ]]; then
+echo "Start Container mongodb"
+ docker container start mongodb
+# echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=mysqlserver )
+if [[ $? -eq 0 ]]; then
+echo "Start Container mysqlserver"
+ docker container start mysqlserver
+# echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=sql1 )
+if [[ $? -eq 0 ]]; then
+echo "Start Container sql1"
+ docker container start sql1
+# echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=salesusernode )
+if [[ $? -eq 0 ]]; then
+echo "Start Container salesusernode"
+ docker container start salesusernode
+# echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=salesproductapi )
+if [[ $? -eq 0 ]]; then
+echo "Start Container salesproductapi"
+ docker container start salesproductapi
+# echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=orderapi )
+if [[ $? -eq 0 ]]; then
+echo "Start Container orderapi"
+ docker container start orderapi
+# echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+
+
+result=$( docker ps -q -f name=apigetway )
+if [[ $? -eq 0 ]]; then
+echo "Start Container apigetway"
+ docker container start apigetway
+# echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=webproduct )
+if [[ $? -eq 0 ]]; then
+echo "Start Container webproduct"
+ docker container start webproduct
+# echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=salesuserreact )
+if [[ $? -eq 0 ]]; then
+echo "Start Container salesuserreact"
+ docker container start salesuserreact
+# echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=salesorderfrontend )
+if [[ $? -eq 0 ]]; then
+echo "Start Container salesorderfrontend"
+ docker container start salesorderfrontend
+# echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+
+
+
+echo "Finish proccess !!"
---
+++ stopdevcontainers.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+########################################################
+
+## Shell Script to Stop Docker Container, DEV.   
+
+########################################################
+
+
+echo "Start the docker container"
+
+result=$( docker ps -q -f name=mongodb )
+if [[ $? -eq 0 ]]; then
+echo "Stop Container mongodb"
+ docker container stop mongodb
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=mysqlserver )
+if [[ $? -eq 0 ]]; then
+echo "Stop Container mysqlserver"
+ docker container stop mysqlserver
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=sql1 )
+if [[ $? -eq 0 ]]; then
+echo "Stop Container sql1"
+ docker container stop sql1
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=salesusernode )
+if [[ $? -eq 0 ]]; then
+echo "Stop Container salesusernode"
+ docker container stop salesusernode
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=salesproductapi )
+if [[ $? -eq 0 ]]; then
+echo "Stop Container salesproductapi"
+ docker container stop salesproductapi
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=orderapi )
+if [[ $? -eq 0 ]]; then
+echo "Stop Container orderapi"
+ docker container stop orderapi
+else
+echo "No such container"
+fi
+
+
+
+result=$( docker ps -q -f name=apigetway )
+if [[ $? -eq 0 ]]; then
+echo "Stop Container apigetway"
+ docker container stop apigetway
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=webproduct )
+if [[ $? -eq 0 ]]; then
+echo "Stop Container webproduct"
+ docker container stop webproduct
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=salesuserreact )
+if [[ $? -eq 0 ]]; then
+echo "Stop Container salesuserreact"
+ docker container stop salesuserreact
+else
+echo "No such container"
+fi
+
+
+result=$( docker ps -q -f name=salesorderfrontend )
+if [[ $? -eq 0 ]]; then
+echo "Stop Container salesorderfrontend"
+ docker container stop salesorderfrontend
+else
+echo "No such container"
+fi
+
+echo "Finish proccess !!"
